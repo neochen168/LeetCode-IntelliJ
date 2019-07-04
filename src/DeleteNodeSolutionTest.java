0a9d9b16b79d51d24264fd1547c5a416d39bf413
ahan1874 <ahan1874@outlook.com>
2019-07-04 15:46:16 +0800

    Add deleteNode solution

---
+++ src/DeleteNodeSolutionTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteNodeSolutionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void deleteNodeFirst() {
+        ListNode header = new ListNode(5);
+        header.next = new ListNode(4);
+        header.next.next = new ListNode(3);
+        header.next.next.next = new ListNode(2);
+        header.next.next.next.next = new ListNode(1);
+        DeleteNodeSolution sl = new DeleteNodeSolution();
+        sl.deleteNode(header);
+
+        ListNode cursor = header;
+        while(cursor!= null){
+            System.out.println(cursor.val);
+            cursor = cursor.next;
+        }
+    }
+
+    @Test
+    void deleteNodeMiddle() {
+        ListNode header = new ListNode(5);
+        header.next = new ListNode(4);
+        header.next.next = new ListNode(3);
+        header.next.next.next = new ListNode(2);
+        header.next.next.next.next = new ListNode(1);
+        DeleteNodeSolution sl = new DeleteNodeSolution();
+        sl.deleteNode(header.next.next);
+
+        ListNode cursor = header;
+        while(cursor!= null){
+            System.out.println(cursor.val);
+            cursor = cursor.next;
+        }
+    }
+
+    @Test
+    void deleteNodeLast() {
+        ListNode header = new ListNode(5);
+        header.next = new ListNode(4);
+        header.next.next = new ListNode(3);
+        header.next.next.next = new ListNode(2);
+        header.next.next.next.next = new ListNode(1);
+        DeleteNodeSolution sl = new DeleteNodeSolution();
+        sl.deleteNode(header.next.next.next.next);
+
+        ListNode cursor = header;
+        while(cursor!= null){
+            System.out.println(cursor.val);
+            cursor = cursor.next;
+        }
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -3,33 +3,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class Solution {
-    /*public int[] twoSum(int[] nums, int target) {
-        int[] origin = nums.clone();
-        Arrays.sort(nums);
-        int start = 0;
-        int end = nums.length-1;
-        int found = 0;
-        while(target != nums[start]+ nums[end]){
-            found = quickFind(start+1, end, target-nums[start], nums);
-            if(found != -1) break;
-            start++;
-        }
-
-
-        int[] ret = new int[2];
-        if(target == nums[start]+nums[found]){
-            for(int i = 0; i < nums.length; i++){
-                if(nums[start] == origin[i]) ret[0] = i;
-
-            }
-            for(int i = nums.length-1; i >= 0; i--){
-                if(nums[found] == origin[i]) ret[1] = i;
-            }
-        }
-        return ret;
-
-    }*/
-
     public int[] twoSum(int[] nums, int target){
         HashMap<Integer, Integer> dic = new HashMap();
 
@@ -90,15 +63,6 @@ public class Solution {
         }
 
         return null;
-    }
-
-    public static void main(String[] args){
-        int test[] = {230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789};
-
-        Solution slo = new Solution();
-        int[] ret = slo.twoSum(test, 524);
-
-        System.out.println(ret[0]);
     }
 }
 
