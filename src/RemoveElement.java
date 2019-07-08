9fc84b12d6ed02174ce1aff35aced2dff915f5b2
ahan1874 <ahan1874@outlook.com>
2019-07-08 19:00:19 +0800

    Remove element solution one time pass and submit

---
+++ src/RemoveElement.java
@@ -0,0 +1,13 @@
+
+public class RemoveElement{
+    public int removeElement(int[] nums, int val) {
+        int fast = 0;
+        int slow = 0;
+        for (; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+}
