dd8eb2e2086cfbe69eee22c504b74fa745cdd777
ahan1874 <ahan1874@outlook.com>
2019-09-29 14:26:34 +0800

    Added DeletedNodeSolution.java

---
+++ src/DeleteNodeSolution.java
@@ -0,0 +1,14 @@
+
+
+class DeleteNodeSolution {
+    public void deleteNode(ListNode node) {
+
+        if(node.next == null){
+            node = null;
+            return;
+        }
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
